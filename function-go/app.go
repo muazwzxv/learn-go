ad0ed0ff9ad699a3732af34e903714406ead4564
muaz <muazwazir@gmail.com>
2020-05-30 01:58:26 +0800

    functions in go

---
+++ function-go/app.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"fmt"
+	"errors"
+)
+
+/*
+To note that golang does not support function overloading
+function with similar role must have different names and
+cannot be overloaded
+*/
+
+// function with one return type
+func concatString(first string, last string) (string) {
+	return first + " " + last
+}
+
+// function with multiple return type
+func concatStringWithError(first string, last string) (string, error) {
+	if first == "" || last == "" {
+		return " ",  errors.New("One of the strings is empty")
+	}
+
+	return first + " " + last, nil
+}
+
+func main() {
+	fmt.Println(concatString("Muaz", "wazir"))
+}
