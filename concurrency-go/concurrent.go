73b0c64dc89164311a87eaacd6ccfe0b49f95cff
muaz <muazwazir@gmail.com>
2020-06-06 03:36:07 +0800

    anonymous goroutine function

--- concurrency-go/concurrent.go
+++ concurrency-go/concurrent.go
@@ -19,6 +19,14 @@ func main() {
 	go timely(10)
     go timely(10)
 	
+	// Go anonymous function
+	go func() {
+		word := "Keeping the concurrency running"
+
+		for i:= 0 ; i < 20 ; i++ {
+			fmt.Println(word, i)
+		}
+	}()
 	// We are expecting an input to terminate the program
 	var input string
 	fmt.Scanln(&input)
