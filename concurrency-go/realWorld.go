078efffcd65e98d91ab5930ea395c67b3e76d814
muaz <muazwazir@gmail.com>
2020-06-06 04:35:14 +0800

    real use

---
+++ concurrency-go/realWorld.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+var urls = []string{
+	"https://google.com",
+	"https://twitter.com",
+	"https://github.com",
+}
+
+func fetch(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res.Status)
+}
+
+func home(w http.ResponseWriter, r *http.Response) {
+	fmt.Println("Homepage endpoint hit boiss")
+	
+	for _, url := range urls {
+		go fetch(url)
+	}
+	fmt.Println("Returning response")
+	fmt.Fprintf(w, "All response received")
+}
+
+func handler() {
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handler()
+}
