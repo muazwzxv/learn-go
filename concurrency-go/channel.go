9cfd13eb8862cfd89114b5ef36532e0efb723681
muaz <muazwazir@gmail.com>
2020-06-07 02:52:43 +0800

    fix

--- concurrency-go/channel.go
+++ concurrency-go/channel.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math/rang"
+	"math/rand"
 )
 
 func CalculateValue(values chan int) {
-	data:= rand.Intn(10)
-	fmt.Println("Calculated Random Value {} ", values)
+	data:= rand.Intn(100)
+	fmt.Println("Calculated Random Value", data)
 	values <- data
 }
 
 func main() {
 	fmt.Println("Go channel testrun")
-
+	
+	// use make function to create a channel
 	values := make(chan int)
 	defer close(values)
 
 	go CalculateValue(values)
+
 	value := <-values
-	fmt.Println(value)
+	fmt.Println("Result returned from Channel", value)
 }
