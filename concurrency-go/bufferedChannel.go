aae3c84efdc6eadd2d89d9c229a17da679d0015a
muaz <muazwazir@gmail.com>
2020-06-07 03:35:58 +0800

    buffered channel

---
+++ concurrency-go/bufferedChannel.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func Calculate(c chan int) {
+	data := rand.Intn(100)
+	fmt.Println("Random value generated", data)
+	time.Sleep(1000 * time.Millisecond)
+	c <- data
+	fmt.Println("This will execute everytime")
+}
+
+func main() {
+	fmt.Println("Buffered Channel Tutorial")
+
+	channel := make(chan int, 2)
+	defer close(channel)
+
+	go Calculate(channel)
+	go Calculate(channel)
+
+	data := <-channel
+	fmt.Println("Return data from channel", data)
+
+	time.Sleep(1000 * time.Millisecond)
+}
