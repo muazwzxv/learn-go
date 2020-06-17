dfeda4fe585383d2dccfd848cfd4c9f3d9a86ab1
muaz <muazwazir@gmail.com>
2020-06-18 00:10:11 +0800

    added mux router

--- rest/main.go
+++ rest/main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
+
 	"github.com/gorilla/mux"
 )
 
-
 type Article struct {
-	Title string `json:"Title"`
+	Title       string `json:"Title"`
 	Description string `json:"Description"`
-	Content string `json:"content"`
+	Content     string `json:"content"`
 }
-
 
 // Create a global Article array
 // Store data in memory, emulate effect of a database
 var Articles []Article
-
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Hompage boi")
@@ -32,14 +30,16 @@ func getAllArticles(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequest() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/api/Articles", getAllArticles)
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", home)
+	router.HandleFunc("/api/Articles", getAllArticles)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-
 func main() {
-	Articles = []Article {
+	Articles = []Article{
 		Article{"Awakening Of Arep", "A chinese boy waking up from sleeping", "Sikes hes gay"},
 		Article{"Star Wars Force Awaken", "A gurl who discover a cult", "Sikes shes  gay"},
 		Article{"Star Wars Return OF The Jedi", "A rise of an ancient cult", "Sikes hes gay"},
