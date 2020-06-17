8f729c10292b1809ac353d1ec03f9e6c8598542b
muaz <muazwazir@gmail.com>
2020-06-18 02:23:18 +0800

    added getArticleByid

--- rest/main.go
+++ rest/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	ID          int    `json:"ID"`
 	Title       string `json:"Title"`
 	Description string `json:"Description"`
 	Content     string `json:"Content"`
@@ -29,21 +32,35 @@ func getAllArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	data := mux.Vars(r)
+	id, err := strconv.Atoi(data["id"])
+	if err != nil {
+		errors.New("Error occured")
+	}
+
+	for _, article := range Articles {
+		if article.ID == id {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
 func handleRequest() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", home)
 	router.HandleFunc("/api/Articles", getAllArticles)
-
+	router.HandleFunc("/api/Article/{id}", getArticle)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func main() {
 	Articles = []Article{
-		Article{"Awakening Of Arep", "A chinese boy waking up from sleeping", "Sikes hes gay"},
-		Article{"Star Wars Force Awaken", "A gurl who discover a cult", "Sikes shes  gay"},
-		Article{"Star Wars Return OF The Jedi", "A rise of an ancient cult", "Sikes hes gay"},
-		Article{"Harry Potter Goblet Of Fire", "A nerd with magic wands", "Sikes hes gay"},
+		Article{1, "Awakening Of Arep", "A chinese boy waking up from sleeping", "Sikes hes gay"},
+		Article{2, "Star Wars Force Awaken", "A gurl who discover a cult", "Sikes shes  gay"},
+		Article{3, "Star Wars Return OF The Jedi", "A rise of an ancient cult", "Sikes hes gay"},
+		Article{4, "Harry Potter Goblet Of Fire", "A nerd with magic wands", "Sikes hes gay"},
 	}
 	fmt.Println("Rest APi v2.0 - Mux Routers")
 	handleRequest()
