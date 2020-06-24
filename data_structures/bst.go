637bc51ea317f4c4c3aa277daa84eb25ef3e6abb
muaz <muazwazir@gmail.com>
2020-06-25 00:57:20 +0800

    tree printer

--- data_structures/bst.go
+++ data_structures/bst.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type node struct {
 	data  int
 	left  *node
@@ -10,9 +12,41 @@ type tree struct {
 	root *node
 }
 
-func max(a, b int) int {
+func max(a int, b int) int {
 	if a > b {
 		return a
 	}
 	return b
+}
+
+func newNode(data int) *node {
+	node := &node{data, nil, nil}
+	return node
+}
+
+func printInOrder(node *node) {
+	if node == nil {
+		return
+	}
+	printInOrder(node.left)
+	fmt.Println(node.data, " ")
+	printInOrder(node.right)
+}
+
+func printPreOrder(node *node) {
+	if node == nil {
+		return
+	}
+	fmt.Println(node.data, " ")
+	printPreOrder(node.left)
+	printPreOrder(node.right)
+}
+
+func printPostOrder(node *node) {
+	if node == nil {
+		return
+	}
+	printPostOrder(node.left)
+	printPostOrder(node.left)
+	fmt.Println(node.data, " ")
 }
