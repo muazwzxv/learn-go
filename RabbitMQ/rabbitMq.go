bdc8421839ccb0635bdf746e09b7428b09f6114f
Muaz Wazir <muazwazir@gmail.com>
2021-02-25 23:26:37 +0800

    rabbitmq connection instance

--- RabbitMQ/rabbitMq.go
+++ RabbitMQ/rabbitMq.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/streadway/amqp"
 )
 
 func main() {
-	fmt.Println("Print")
+	fmt.Println("RabbitMQ + Go")
+	con, err := amqp.Dial("ampq://guest:guest@localhost5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(1)
+	}
+
+	defer con.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ instance")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/muazwzxv/learn-go
 
 go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/streadway/amqp v1.0.0 // indirect
+)
