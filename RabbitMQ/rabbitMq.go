a4c9457b395c588c7358f6b2f0c48b8b0057f088
Muaz Wazir <muazwazir@gmail.com>
2021-02-26 00:40:57 +0800

    queue declare

--- RabbitMQ/rabbitMq.go
+++ RabbitMQ/rabbitMq.go
@@ -8,13 +8,30 @@ import (
 
 func main() {
 	fmt.Println("RabbitMQ + Go")
-	con, err := amqp.Dial("ampq://guest:guest@localhost5672/")
+	con, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		fmt.Println(err)
 		panic(1)
 	}
 
+	// opens a channel to RabbitMQ instance
+	ch, err := con.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer con.Close()
+
+	// interact with the instance and declare a queue
+	// we can publish and subscribe to
+	queue, err := ch.QueueDeclare(
+		"test", // name
+		true,   // durable
+		false,  // exclusive
+		false,  // autoDelete
+		false,  //
+		nil,
+	)
+	fmt.Println(queue)
 
 	fmt.Println("Successfully connected to RabbitMQ instance")
 }
