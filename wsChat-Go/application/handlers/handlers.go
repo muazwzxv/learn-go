5185c8c394cb350791d0923dd56ae8a814a1ce97
Muaz Wazir <muazwazir@gmail.com>
2021-09-01 00:23:24 +0800

    set up templating engine using jet

---
+++ wsChat-Go/application/handlers/handlers.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"github.com/CloudyKit/jet/v6"
+	"log"
+	"net/http"
+)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode())
+
+func Home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func renderPage(w http.ResponseWriter, template string, data jet.VarMap) error {
+	view, err := views.GetTemplate(template)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
