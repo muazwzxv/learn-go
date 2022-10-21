66f4643a3b7fca288bfff21c221ef2d375534085
muashh <muazwazir@gmail.com>
2022-10-21 20:44:18 +0800

    go routine

---
+++ concurrency-part2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	go run()
+
+	for i := 0; i < 10; i++ {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("outside goroutine")
+	}
+
+	defer func() {
+		fmt.Println("execution finished")
+	}()
+
+}
+
+func run() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("inside goroutine")
+	}
+}
