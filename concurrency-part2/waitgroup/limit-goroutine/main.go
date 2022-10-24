2604df30294caf08aacdf4c608859c5919f3d08e
muashh <muazwazir@gmail.com>
2022-10-24 17:19:52 +0800

    rate limiting by batch with waitgroup

---
+++ concurrency-part2/waitgroup/limit-goroutine/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type request func()
+
+func main() {
+	requests := map[int]request{}
+
+	for i := 1; i <= 100; i++ {
+		f := func(n int) request {
+			return func() {
+				fmt.Println("request: ", n)
+			}
+		}
+
+		requests[i] = f(i)
+	}
+
+	var wg sync.WaitGroup
+	max := 10
+
+	for i := 0; i < len(requests); i += max {
+		for j := i; j < i+max; j++ {
+			wg.Add(1)
+			go func(f request) {
+				defer wg.Done()
+				f()
+			}(requests[j+1])
+		}
+		wg.Wait()
+		fmt.Println(max, "request processed")
+	}
+}
