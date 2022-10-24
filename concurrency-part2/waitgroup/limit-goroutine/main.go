3741c62d33b2c3e96f8f9904e6583cd7af7a79d6
muashh <muazwazir@gmail.com>
2022-10-24 18:01:02 +0800

    minor improvements

--- concurrency-part2/waitgroup/limit-goroutine/main.go
+++ concurrency-part2/waitgroup/limit-goroutine/main.go
@@ -19,19 +19,25 @@ func main() {
 
 		requests[i] = f(i)
 	}
+	fmt.Println(requests)
 
 	var wg sync.WaitGroup
 	max := 10
 
-	for i := 0; i < len(requests); i += max {
-		for j := i; j < i+max; j++ {
+	fmt.Printf("\n before execution: \n %v", requests)
+	loop := len(requests)
+	for i := 0; i < loop; i += max {
+		for j := i; j < (i + max); j++ {
 			wg.Add(1)
 			go func(f request) {
 				defer wg.Done()
 				f()
 			}(requests[j+1])
+			delete(requests, j+1)
 		}
 		wg.Wait()
 		fmt.Println(max, "request processed")
 	}
+
+	fmt.Printf("\n after execution: \n %v", requests)
 }
